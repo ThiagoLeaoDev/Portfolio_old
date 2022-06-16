e32084a9065163b03f3bc186c12db5d2f43171d5
Thiago Leão <45953201+ThiagoLeaoDev@users.noreply.github.com>
2022-06-16 12:43:33 -0300

    authorization test

--- components/CardProjects/index.js
+++ components/CardProjects/index.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import * as SC from './style'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import * as SC from "./style";
 import { BiLayout, BiCodeBlock, BiMobile, BiRocket } from "react-icons/bi";
 
 export default function index(props) {
@@ -8,50 +8,58 @@ export default function index(props) {
 
   useEffect(() => {
     const getProjects = async () => {
-      const {data: res} = await axios.get("https://api.figma.com/v1/files/", {
-        headers: {
-          authorization: `Bearer ${process.env.FIGMA_KEY}`
-        }
-          });
-      setProjects(res)
-    }
+      const { data: res } = await axios
+        .get("https://api.figma.com/v1/files/", {
+          headers: {
+            Authorization: `token ${process.env.FIGMA_KEY}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setProjects(res);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
     getProjects();
     console.log(projects);
-  }
-  , []);
-  
+  }, []);
+
   return (
     <SC.ContainerCard>
       <SC.ContainerImage>
-        <SC.ProjectImage src={props.image} alt='Project Image' layout='fill' objectFit="cover" quality={100}/>
+        <SC.ProjectImage
+          src={props.image}
+          alt="Project Image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
       </SC.ContainerImage>
 
       <SC.CardText>
         <SC.Title>{props.Title}</SC.Title>
         <SC.ContainerTypeProject>
-          {
-            props.Type == 'code' ?
-              <BiCodeBlock size="1.5em" color="#13FF00"/>
-            : props.Type == 'ui' ?
-              <BiLayout size="1.5em" color="#13FF00"/>
-            : props.Type == 'mobile' ?
-              <BiMobile size="1.5em" color="#13FF00"/>
-            : props.Type == 'social' ?
-              <BiRocket size="1.5em" color="#13FF00"/>
-            : null
-          }
+          {props.Type == "code" ? (
+            <BiCodeBlock size="1.5em" color="#13FF00" />
+          ) : props.Type == "ui" ? (
+            <BiLayout size="1.5em" color="#13FF00" />
+          ) : props.Type == "mobile" ? (
+            <BiMobile size="1.5em" color="#13FF00" />
+          ) : props.Type == "social" ? (
+            <BiRocket size="1.5em" color="#13FF00" />
+          ) : null}
           <SC.TypeName>
-            {
-              props.Type == 'code' ?
-                'Web Development'
-              : props.Type == 'ui' ?
-                'UI Design'
-              : props.Type == 'mobile' ?
-                'Mobile Development'
-              : props.Type == 'social' ?
-                'Social Media Design'
-              : null 
-            }
+            {props.Type == "code"
+              ? "Web Development"
+              : props.Type == "ui"
+              ? "UI Design"
+              : props.Type == "mobile"
+              ? "Mobile Development"
+              : props.Type == "social"
+              ? "Social Media Design"
+              : null}
           </SC.TypeName>
         </SC.ContainerTypeProject>
       </SC.CardText>
@@ -59,12 +67,12 @@ export default function index(props) {
       <SC.ContainerButtons>
         <SC.ButtonView>
           <SC.ButtonTextView>View Code</SC.ButtonTextView>
-          <SC.ArrowIcon size="1.5em" color="#FFFFFF"/>
+          <SC.ArrowIcon size="1.5em" color="#FFFFFF" />
         </SC.ButtonView>
         <SC.ButtonLive>
           <SC.ButtonTextLive>Live Preview</SC.ButtonTextLive>
         </SC.ButtonLive>
       </SC.ContainerButtons>
     </SC.ContainerCard>
-  ) 
+  );
 }
